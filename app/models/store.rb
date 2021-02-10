d83c89f0137dacdd8ead3385214fdba8647f8039
yuta <az7xj5xn@yahoo.co.jp>
2021-02-10 17:40:09 +0900

    bcryptのインストール、Userモデルのバリデーション設定

--- app/models/store.rb
+++ app/models/store.rb
@@ -5,4 +5,5 @@ class Store < ApplicationRecord
   has_many :active_follows, foreign_key: 'follower_id', class_name: 'StoreFavorite', dependent: :destroy
   has_many :passive_follows, foreign_key: 'followed_id', class_name: 'StoreFavorite', dependent: :destroy
   has_many :following_stores, through: :active_follows, source: :followed
+  has_secure_password
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,12 @@ class User < ApplicationRecord
   has_many :user_favorites, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :favorite_stores, through: :user_favorites, source: :store
+
+  validates :name, presence: true, length: { maximum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }, format: {
+    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  }
+  validates :password, presence: true
+  before_validation { email.downcase! if email.present? }
+  has_secure_password
 end
