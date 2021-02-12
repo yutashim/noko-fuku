1d5645221e51608c9e5cd36b3241121d224cdf8b
yuta <az7xj5xn@yahoo.co.jp>
2021-02-12 14:50:20 +0900

    SessionsHelperを編集

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,4 @@
 class SessionsController < ApplicationController
-  MODELS = { user_session: User, store_session: Store }
-
   def new
   end
 
@@ -10,11 +8,7 @@ class SessionsController < ApplicationController
   def create
     key = params.keys.find{ |n| n=~ /.+_session/}.to_sym
     pr = params[key]
-    login_user = MODELS[key].find_by(email: pr[:email])
-    if login_user && login_user.authenticate(pr[:password])
-      session[key] = login_user.id
-    end
-    @login_user = login_user
+    login(key, pr)
   end
 
   def destroy
