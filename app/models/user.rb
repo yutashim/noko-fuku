259a5209fcd706387af15b4e30472186d44297f7
yuta <az7xj5xn@yahoo.co.jp>
2021-02-12 10:59:37 +0900

    Storeモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   has_many :favorite_stores, through: :user_favorites, source: :store
 
   validates :name, presence: true, length: { maximum: 255 }
-  validates :email, presence: true, length: { maximum: 255 }, format: {
-    with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  }
-  validates :password, presence: true
+  validates :email, presence: true, length: { maximum: 255 },
+    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
+  validates :password, presence: true, length: { maximum: 255 }
   before_validation { email.downcase! if email.present? }
   has_secure_password
 end
