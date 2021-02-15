835782c9e24755b75b5901b0d35a724fc997c8d9
yuta <az7xj5xn@yahoo.co.jp>
2021-02-15 17:10:09 +0900

    promotionの投稿機能を追加

---
+++ app/controllers/promotions_controller.rb
@@ -0,0 +1,23 @@
+class PromotionsController < ApplicationController
+  before_action :set_store, only: [:new, :create]
+  def new
+    @promotion = Promotion.new
+  end
+
+  def create
+    @promotion = @store.promotions.build(promotion_params)
+    if @promotion.save
+      redirect_to store_path(@promotion.store_id)
+    else
+      render :new
+    end
+  end
+  private
+  def set_store
+    @store = Store.find(params[:store_id])
+  end
+
+  def promotion_params
+    params.require(:promotion).permit(:title, :content, :store_id)
+  end
+end
