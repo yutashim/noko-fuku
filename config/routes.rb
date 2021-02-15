529a8da69def040ca9760c7d89d4acea2325e05b
yuta <az7xj5xn@yahoo.co.jp>
2021-02-15 10:28:42 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   get '/store_login', to: 'sessions#store_login'
-
+  resources :stores do
+    resources :comments
+    resources :promotions
+  end
+  resources :user_favorites, only: [:create, :destroy]
+  resources :store_favorites, only: [:create, :destroy]
+  resources :users
 end
