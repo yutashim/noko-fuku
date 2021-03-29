33be15e6a7ac5686c99e14ad1d468551481c6b28
yuta <az7xj5xn@yahoo.co.jp>
2021-03-29 19:43:11 +0900

    add_column~をコメントアウト

--- db/migrate/20210329103820_add_guest_to_stores.rb
+++ db/migrate/20210329103820_add_guest_to_stores.rb
@@ -1,5 +1,5 @@
 class AddGuestToStores < ActiveRecord::Migration[5.2]
   def change
-    add_column :stores, :guest, :boolean, default: false, null: false
+    # add_column :stores, :guest, :boolean, default: false, null: false
   end
 end
